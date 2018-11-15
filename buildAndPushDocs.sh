3e4685359bed9fa41098732109814d6a05d6031c
blank <bakareemmanuel98@outlook.com>
2018-11-15 11:26:24 +0100

    Added new changes

---
+++ buildAndPushDocs.sh
@@ -0,0 +1,8 @@
+
+docker-compose exec docs sh -c "cd .. ; bundle exec middleman build; rm -r /app/source/build; mv /app/build/ /app/source"
+
+git add *
+
+git commit -am "Added new changes"
+
+cd .. && git subtree push --prefix slate-docker/build origin gh-pages
